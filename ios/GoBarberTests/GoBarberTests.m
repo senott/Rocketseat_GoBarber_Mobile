064896f2e1270ce6675cdb101660b4e6b047a2fc
Senott <senott@yahoo.com>
2020-09-09 15:53:53 -0300

    Project renamed.

--- ios/GoBarberTests/GoBarberTests.m
+++ ios/GoBarberTests/GoBarberTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativetemplateTests : XCTestCase
+@interface GoBarberTests : XCTestCase
 
 @end
 
-@implementation reactnativetemplateTests
+@implementation GoBarberTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
